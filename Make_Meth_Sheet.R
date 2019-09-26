e34ea2b98be4f1df2e907f6543697a69ee0a00ae
bgudenas <bgudena@g.clemson.edu>
2019-09-26 15:41:08 -0500

    Make meth sheet for minfi

---
+++ Make_Meth_Sheet.R
@@ -0,0 +1,23 @@
+
+# 
+ dir = "./Methylation/IDATS/ETMRlikeDICER/"
+ outname = "./Methylation/IDATS/ETMRlikeDICER/newRefs_meta.csv"
+ group = "450k"
+
+makeMethSheet = function(dir, group, outname){
+  idats = list.files(dir)
+  idats = idats[grepl("idat", idats)]
+  
+  isplit = stringr::str_split(idats, "_")
+  samps = paste0(unlist(lapply(isplit, "[[", 1)), "_",unlist(lapply(isplit, "[[", 2)))
+  
+  df = data.frame("Sample_Name" = samps, "Sample_Group" = group, status = "cancer", 
+                  "Slide" = unlist(lapply(isplit, "[[", 1)),
+                  "Array" = unlist(lapply(isplit, "[[", 2)))
+  
+  df =df[!duplicated(df$Sample_Name), ]
+  
+  write.csv(df, outname, quote = FALSE, row.names = FALSE)
+}
+
+makeMethSheet(dir, group, outname)  
