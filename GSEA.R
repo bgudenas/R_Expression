41014c59e908f1c2310e8def47aa96b544c3bd26
bgudenas <bgudena@g.clemson.edu>
2020-07-07 14:36:11 -0500

    Added checks if top genesets is less than expected

--- GSEA.R
+++ GSEA.R
@@ -1,4 +1,4 @@
-GSEA = function(ranked, GO_file ) {
+GSEA = function(ranked, GO_file, pval = 0.05, display_num = 10) {
   set.seed(54321)
   library(gage)
   library(fgsea)
@@ -16,19 +16,19 @@ GSEA = function(ranked, GO_file ) {
                            maxSize=400,
                            nperm=10000)
   
-  fgseaRes = fgseaRes[fgseaRes$padj < 0.05, ]
+  fgseaRes = fgseaRes[fgseaRes$padj <= 0.05, ]
   
   ## Filter FGSEA by using gage results. Must be significant and in same direction to keep 
   gobpres = gage(rnks, gsets=myGO, same.dir=TRUE, set.size =c(15,400))
   
   ups = as.data.frame(gobpres$greater) %>% 
     tibble::rownames_to_column("Pathway") %>% 
-    dplyr::filter(!is.na(p.geomean) & q.val < 0.05 ) %>%
+    dplyr::filter(!is.na(p.geomean) & q.val <= 0.05 ) %>%
     dplyr::select("Pathway")
   
   downs = as.data.frame(gobpres$less) %>% 
     tibble::rownames_to_column("Pathway") %>% 
-    dplyr::filter(!is.na(p.geomean) & q.val < 0.05 ) %>%
+    dplyr::filter(!is.na(p.geomean) & q.val <= 0.05 ) %>%
     dplyr::select("Pathway")
   
   tmpup = fgseaRes[fgseaRes$NES > 0 & !is.na(match(fgseaRes$pathway, ups$Pathway)), ]
@@ -37,23 +37,37 @@ GSEA = function(ranked, GO_file ) {
   
   ### Collapse pathways
   keepsUp = fgsea::collapsePathways(filtRes[filtRes$NES > 1.5, ], myGO, rnks,  nperm = 500)
-  keepsDown = fgsea::collapsePathways(filtRes[filtRes$NES < -1.5, ], myGO, rnks,  nperm = 500)
+  keepsDown = fgsea::collapsePathways(filtRes[filtRes$NES < -1,5, ], myGO, rnks,  nperm = 500)
   keeps = c(keepsUp$mainPathways, keepsDown$mainPathways)
   filtRes = filtRes[ !is.na(match(filtRes$pathway, keeps)), ] %>% 
     arrange(desc(NES))
   
+  display_num =
+  ## if all same sign select top 10 or as many as available if split sign select top 10 for each or top pos,neg available
+  if (  all(filtRes$NES > 0 ) | all(filtRes$NES < 0 ) ){
+    
+    sel_sets = ifelse( nrow(filtRes) >= display_num, display_num, nrow(filtRes))
+   filtResTidy  =  filtRes %>%
+     arrange(desc(abs(NES))) %>% 
+     top_n(n = sel_sets, wt = abs(NES))
+  } else {
+    NES_logvec = filtRes$NES > 0
+    sel_pos = ifelse( nrow(filtRes[NES_logvec, ]) >= display_num, display_num, nrow(filtRes[NES_logvec, ]))
+    sel_neg = ifelse( nrow(filtRes[!NES_logvec, ]) >= display_num, display_num, nrow(filtRes[!NES_logvec, ]))
+    
+    filtResTidy  = rbind(filtRes[1:sel_pos, ],
+                         filtRes[(nrow(filtRes) - sel_neg + 1 ):nrow(filtRes),  ])
+  }
   
-  filtResTidy = rbind(head(filtRes, n = 10), tail(filtRes, n = 10))
   filtResTidy$Enrichment = ifelse(filtResTidy$NES > 0, "Up-regulated", "Down-regulated")
-  colvec =  c("blue3", "red3")
-  names(colvec) = c("Down-regulated", "Up-regulated")
+  colvec = setNames(object = c("blue3", "red3"), nm = c("Down-regulated", "Up-regulated"))
   
   g = ggplot(filtResTidy,  aes(reorder(pathway, NES), NES)) +
     geom_col(aes(fill = Enrichment)) +
     scale_fill_manual(values= colvec) +
     coord_flip() +
     labs(x="Pathway", y="Normalized Enrichment Score",
-         title="GSEA - Biological Processes (top 10)") + 
+         title="GSEA - Biological Processes") + 
     theme_minimal()
   
   output = list("Results" = filtRes, "Plot" = g)
