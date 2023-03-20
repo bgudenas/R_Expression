c055453aa9b4b3d01bfa521f1cf7c3055a71fe53
bgudenas <bgudena@g.clemson.edu>
2023-03-20 11:34:50 -0500

    update

--- makeAnnotables.R
+++ makeAnnotables.R
@@ -3,16 +3,19 @@
 ## Make gene annotation tables for different species containing gene-level info
 
 MakeHgAnno = function(path){
+  library(biomaRt)
   dataset = "hsapiens_gene_ensembl"
   mart = biomaRt::useMart("ensembl", dataset = dataset )
-  map = biomaRt::getBM(mart = mart, attributes=c("ensembl_gene_id","entrezgene","external_gene_name","hgnc_symbol", "gene_biotype", 
+  map = biomaRt::getBM(mart = mart, attributes=c("ensembl_gene_id","external_gene_name","hgnc_symbol", "gene_biotype", 
                                                  "chromosome_name", "start_position", "end_position","description",
                                                  "transcript_count", "strand", "band" ))
   
-  map2 = biomaRt::getBM(mart = mart, attributes=c("ensembl_gene_id",'mmusculus_homolog_ensembl_gene', "mmusculus_homolog_associated_gene_name" ) )
+  map2 = biomaRt::getBM(mart = mart, attributes=c("ensembl_gene_id", 'mmusculus_homolog_ensembl_gene', "mmusculus_homolog_associated_gene_name" ) )
+  map3 = biomaRt::getBM(mart = mart, attributes=c("ensembl_gene_id","entrezgene_id"))
   
   map$mmusculus_homolog_associated_gene_name = map2$mmusculus_homolog_associated_gene_name[ match(map$ensembl_gene_id, map2$ensembl_gene_id) ]
   map$mmusculus_homolog_ensembl_gene = map2$mmusculus_homolog_ensembl_gene[ match(map$ensembl_gene_id, map2$ensembl_gene_id) ]
+  map$entrezgene_id = map3$entrezgene_id[ match(map$ensembl_gene_id, map3$ensembl_gene_id) ]
 
   saveRDS(map,  path )
 }
