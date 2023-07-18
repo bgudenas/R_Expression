f1c39f362fb33b6b576e6ff47b75f11964edf11e
bgudenas <bgudena@g.clemson.edu>
2023-07-18 12:27:51 -0500

    update GSEA plot

--- GSEA.R
+++ GSEA.R
@@ -1,4 +1,4 @@
-GSEA = function(ranked, GO_file, pval = 0.05, display_num = 10) {
+GSEA = function(ranked, GO_file, pval = 0.05, display_num = 10, UP_only = FALSE) {
   set.seed(54321)
   library(gage)
   library(fgsea)
@@ -8,13 +8,17 @@ GSEA = function(ranked, GO_file, pval = 0.05, display_num = 10) {
   rnks = rnks[!duplicated(names(rnks))]
   rnks = sort(rnks, decreasing = TRUE)
   
-  myGO = fgsea::gmtPathways(GO_file)
+  if (is.character(GO_file)){
+    myGO = fgsea::gmtPathways(GO_file)
+  } else {
+    myGO = GO_file 
+  }
   
   fgseaRes <- fgsea::fgsea(pathways = myGO, 
                            stats = rnks,
                            minSize=15,
                            maxSize=400,
-                           nperm=10000)
+                           nperm=100000)
   
   fgseaRes = fgseaRes[fgseaRes$padj <= 0.05, ]
   
@@ -37,12 +41,11 @@ GSEA = function(ranked, GO_file, pval = 0.05, display_num = 10) {
   
   ### Collapse pathways
   keepsUp = fgsea::collapsePathways(filtRes[filtRes$NES > 1.5, ], myGO, rnks,  nperm = 500)
-  keepsDown = fgsea::collapsePathways(filtRes[filtRes$NES < -1,5, ], myGO, rnks,  nperm = 500)
+  keepsDown = fgsea::collapsePathways(filtRes[filtRes$NES < -1.5, ], myGO, rnks,  nperm = 500)
   keeps = c(keepsUp$mainPathways, keepsDown$mainPathways)
   filtRes = filtRes[ !is.na(match(filtRes$pathway, keeps)), ] %>% 
     arrange(desc(NES))
   
-  display_num =
   ## if all same sign select top 10 or as many as available if split sign select top 10 for each or top pos,neg available
   if (  all(filtRes$NES > 0 ) | all(filtRes$NES < 0 ) ){
     
@@ -60,15 +63,99 @@ GSEA = function(ranked, GO_file, pval = 0.05, display_num = 10) {
   }
   
   filtResTidy$Enrichment = ifelse(filtResTidy$NES > 0, "Up-regulated", "Down-regulated")
-  colvec = setNames(object = c("blue3", "red3"), nm = c("Down-regulated", "Up-regulated"))
+  if (UP_only == TRUE ){ filtResTidy = filtResTidy[ filtResTidy$NES > 0, ] }
+  #colvec = setNames(object = c("blue3", "red3"), nm = c("Down-regulated", "Up-regulated"))
+  upcols =  colorRampPalette(colors = c("red4", "red1", "lightpink"))( sum(filtResTidy$Enrichment == "Up-regulated"))
+  downcols =  colorRampPalette(colors = c( "lightblue", "blue1", "blue4"))( sum(filtResTidy$Enrichment == "Down-regulated"))
+  colos = c(upcols, downcols)
+  names(colos) = 1:length(colos)
+  filtResTidy$Index = as.factor(1:nrow(filtResTidy))
+  filtResTidy$pathway = str_remove(filtResTidy$pathway, "GO_")
+  filtResTidy$pathway = str_replace_all(filtResTidy$pathway, "_", " ")
   
   g = ggplot(filtResTidy,  aes(reorder(pathway, NES), NES)) +
-    geom_col(aes(fill = Enrichment)) +
-    scale_fill_manual(values= colvec) +
+    geom_col(aes(fill = Index)) +
+    scale_fill_manual(values= colos ) +
     coord_flip() +
     labs(x="Pathway", y="Normalized Enrichment Score",
          title="GSEA - Biological Processes") + 
-    theme_minimal()
+    theme_minimal() +
+    theme(legend.position = "none")
+  
+  output = list("Results" = filtRes, "Plot" = g)
+  return(output)
+}
+
+
+
+
+### just fgsea
+
+GSEA_single = function(ranked, GO_file, pval = 0.05, display_num = 10, UP_only = FALSE) {
+  set.seed(54321)
+  library(gage)
+  library(fgsea)
+  library(dplyr)
+  library(stringr)
+  
+  rnks = ranked
+  rnks =rnks[!is.na(names(rnks))]
+  rnks = rnks[!duplicated(names(rnks))]
+  rnks = sort(rnks, decreasing = TRUE)
+  
+  myGO = fgsea::gmtPathways(GO_file)
+  
+  fgseaRes <- fgsea::fgsea(pathways = myGO, 
+                           stats = rnks,
+                           minSize=15,
+                           maxSize=400,
+                           nperm=100000)
+  
+  filtRes = fgseaRes[fgseaRes$padj <= 0.05, ]
+
+  
+  ### Collapse pathways
+  keepsUp = fgsea::collapsePathways(filtRes[filtRes$NES > 1.5, ], myGO, rnks,  nperm = 500)
+  keepsDown = fgsea::collapsePathways(filtRes[filtRes$NES < -1.5, ], myGO, rnks,  nperm = 500)
+  keeps = c(keepsUp$mainPathways, keepsDown$mainPathways)
+  filtRes = filtRes[ !is.na(match(filtRes$pathway, keeps)), ] %>% 
+    arrange(desc(NES))
+  
+  ## if all same sign select top 10 or as many as available if split sign select top 10 for each or top pos,neg available
+  if (  all(filtRes$NES > 0 ) | all(filtRes$NES < 0 ) ){
+    
+    sel_sets = ifelse( nrow(filtRes) >= display_num, display_num, nrow(filtRes))
+    filtResTidy  =  filtRes %>%
+      arrange(desc(abs(NES))) %>% 
+      top_n(n = sel_sets, wt = abs(NES))
+  } else {
+    NES_logvec = filtRes$NES > 0
+    sel_pos = ifelse( nrow(filtRes[NES_logvec, ]) >= display_num, display_num, nrow(filtRes[NES_logvec, ]))
+    sel_neg = ifelse( nrow(filtRes[!NES_logvec, ]) >= display_num, display_num, nrow(filtRes[!NES_logvec, ]))
+    
+    filtResTidy  = rbind(filtRes[1:sel_pos, ],
+                         filtRes[(nrow(filtRes) - sel_neg + 1 ):nrow(filtRes),  ])
+  }
+  
+  filtResTidy$Enrichment = ifelse(filtResTidy$NES > 0, "Up-regulated", "Down-regulated")
+  if (UP_only == TRUE ){ filtResTidy = filtResTidy[ filtResTidy$NES > 0, ] }
+  #colvec = setNames(object = c("blue3", "red3"), nm = c("Down-regulated", "Up-regulated"))
+  upcols =  colorRampPalette(colors = c("red4", "red1", "lightpink"))( sum(filtResTidy$Enrichment == "Up-regulated"))
+  downcols =  colorRampPalette(colors = c( "lightblue", "blue1", "blue4"))( sum(filtResTidy$Enrichment == "Down-regulated"))
+  colos = c(upcols, downcols)
+  names(colos) = 1:length(colos)
+  filtResTidy$Index = as.factor(1:nrow(filtResTidy))
+  filtResTidy$pathway = str_remove(filtResTidy$pathway, "GO_")
+  filtResTidy$pathway = str_replace_all(filtResTidy$pathway, "_", " ")
+  
+  g = ggplot(filtResTidy,  aes(reorder(pathway, NES), NES)) +
+    geom_col(aes(fill = Index)) +
+    scale_fill_manual(values= colos ) +
+    coord_flip() +
+    labs(x="Pathway", y="Normalized Enrichment Score",
+         title="GSEA - Biological Processes") + 
+    theme_minimal() +
+    theme(legend.position = "none")
   
   output = list("Results" = filtRes, "Plot" = g)
   return(output)
